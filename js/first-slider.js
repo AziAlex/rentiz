a7504e0f2f5d17155fe5731ac4703747da76ef14
Alex <102092661+AziAlex@users.noreply.github.com>
2023-04-21 01:33:33 +0300

    init

--- js/first-slider.js
+++ js/first-slider.js
@@ -1,5 +1,5 @@
 
-var swiper = new Swiper(".mySwiper", {
+const swip = {
     slidesPerView: 3,
     spaceBetween: 30,
     slidesPerGroup: 3,
@@ -13,4 +13,41 @@ var swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+}
+
+let width = window.innerWidth
+const slidesCount = width <= 500 ? 1 : width > 500 && width <= 900 ? 2 : 3
+var swiper = new Swiper(".mySwiper", {
+    ...swip,
+    slidesPerView: slidesCount,
+    slidesPerGroup: slidesCount
 });
+
+window.addEventListener("resize", event => {
+    width = window.innerWidth
+    switch (true) {
+        case width < 500:
+            swiper = new Swiper(".mySwiper", {
+                ...swip,
+                slidesPerView: 1,
+                slidesPerGroup: 1,
+            });
+            break;
+        case width >= 500 && width < 900:
+            swiper = new Swiper(".mySwiper", {
+                ...swip,
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+            });
+            break;
+        case width >= 900:
+            swiper = new Swiper(".mySwiper", {
+                ...swip,
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+            });
+            break;
+    }
+})
+
+
